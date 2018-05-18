ceeef95cc4bfccba6099a4f02bf662ea253b36b9
WeiKunpeng <760963903@qq.com>
2018-05-18 17:40:13 +0800

    wei first

---
+++ src/testsocket/Main_GUI.java
@@ -0,0 +1,100 @@
+package testsocket;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class Main_GUI {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main_GUI window = new Main_GUI();
+					window.frame.setVisible(true);
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main_GUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setTitle("\u767B\u5F55\u754C\u9762");
+		frame.setBounds(100, 100, 500, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("\u8D26\u53F7");
+		lblNewLabel.setBounds(108, 86, 48, 20);
+		frame.getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(201, 83, 188, 26);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("\u5BC6\u7801");
+		lblNewLabel_1.setBounds(108, 128, 69, 20);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(201, 125, 188, 23);
+		frame.getContentPane().add(passwordField);
+		
+		JButton button = new JButton("\u767B\u5F55");
+	
+				button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Talk window = new Talk();
+			
+			}
+		});
+		button.setBounds(95, 196, 115, 29);
+		frame.getContentPane().add(button);
+		
+		JButton button_1 = new JButton("\u6CE8\u518C");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Register window = new Register();
+			}
+		});
+		button_1.setBounds(268, 196, 115, 29);
+		frame.getContentPane().add(button_1);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 280, 478, 164);
+		frame.getContentPane().add(scrollPane);
+		
+		JTextArea textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+	}
+}
